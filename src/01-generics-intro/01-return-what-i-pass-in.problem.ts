f4d0dc7bcf98f1379dfdd42f54304b8b77268a58
Leo Jhuo <leo_jhuo@wistron.com>
2023-04-15 17:38:31 +0800

    chore: section 1

--- src/01-generics-intro/01-return-what-i-pass-in.problem.ts
+++ src/01-generics-intro/01-return-what-i-pass-in.problem.ts
@@ -1,10 +1,12 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnWhatIPassIn = (t: unknown) => {
-  return t;
+const returnWhatIPassIn = <T>(t: T) => {
+	return t;
 };
 
 const one = returnWhatIPassIn(1);
+//    ^?
+
 const matt = returnWhatIPassIn("matt");
 
 type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
